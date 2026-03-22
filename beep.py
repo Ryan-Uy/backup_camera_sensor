be03359b765a50999f47e556a87d760b76c0aa11
Ryan-Uy <ryuy123@gmail.com>
2026-03-22 18:35:44 -0400

    Turned the beep into a .wav to simplify the python program.

--- beep.py
+++ beep.py
@@ -1,33 +1,11 @@
 import serial
 import pygame
-import numpy as np
 import time
+import keyboard
 
-pygame.mixer.init(frequency=44100, size=-16, channels=2)
+pygame.mixer.init()
 
-def make_beep(frequency=1000, duration=0.2, volume=0.5):
-    sample_rate = 44100
-    t = np.linspace(0, duration, int(sample_rate * duration), False)
-
-    wave = np.sin(2 * np.pi * frequency * t)
-
-    # fade (envelope)
-    fade_len = int(sample_rate * 0.01)
-    envelope = np.ones_like(wave)
-    envelope[:fade_len] = np.linspace(0, 1, fade_len)
-    envelope[-fade_len:] = np.linspace(1, 0, fade_len)
-
-    wave *= envelope
-
-    audio = (wave * (2**15 - 1) * volume).astype(np.int16)
-
-    # make it stereo: shape becomes (samples, 2)
-    stereo_audio = np.column_stack((audio, audio))
-
-    return pygame.sndarray.make_sound(stereo_audio)
-
-beep = make_beep(1000, 0.15)
-interval = .5
+beep = pygame.mixer.Sound('beep.wav')
 
 with serial.Serial('COM3', 9600, timeout=1) as ser:
     time.sleep(2)
@@ -47,3 +25,7 @@ with serial.Serial('COM3', 9600, timeout=1) as ser:
         if level and now - last_beep_time >= interval[level]:
             beep.play()
             last_beep_time = now
+            
+        if keyboard.is_pressed('q') or keyboard.is_pressed('esc'):
+            break
+        
